8cd0d54c5a202586ede0e21b69f21cff8a1af949
Vincent Mader <vincent.mader@icloud.com>
2020-10-13 03:49:28 +0200

    configured dwm

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,25 +1,34 @@
 /* See LICENSE file for copyright and license details. */
 
+/* Constants (copied from Luke Smith) */
+#define TERMINAL "st"
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 20;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "Font Awesome:size=18" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_green[]       = "#006600";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+    /*               fg         bg         border   */
+    [SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
+    [SchemeSel]  = { col_green, col_gray1, col_green },
 };
 
+/* status bar */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const int user_bh = 40;                  /* height of status bar */
+
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { 
+    "", "", "", "", "", "", "", "", "" 
+};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -32,19 +41,32 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
+#include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	/* { "[]=",      tile },         /1* first entry is default *1/ */
+	/* { "><>",      NULL },         /1* no layout function means floating behavior *1/ */
+	/* { "[M]",      monocle }, */
+	/* { "TTT",      bstack },       /1* alt-u *1/ */
+	/* { "===",      bstackhoriz },  /1* alt-o *1/ */
+  	/* { "[@]",      spiral },       /1* alt-r *1/ */
+  	/* { "[\\]",     dwindle },      /1* alt-shift-r *1/ */
+	{ "",      tile },         /* first entry is default */
+	{ "",      NULL },         /* no layout function means floating behavior */
+	{ "",      monocle },
+	{ "",      bstack },       /* alt-u */
+	{ "",      bstackhoriz },  /* alt-o */
+  	{ "",      spiral },       /* alt-r */
+  	{ "",      dwindle },      /* alt-shift-r */
 };
 
 /* key definitions */
 #define MODKEY Mod1Mask
+#define CMDKEY Mod4Mask  /* Mod4Mask  = cmd key */ 
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -56,11 +78,28 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+
+	{ CMDKEY,                       XK_w,      spawn,          SHCMD(TERMINAL " -e nvim -u $CONF/nvim/init.vim $DROPBOX/vimwiki/index.wiki") },
+	{ CMDKEY,                       XK_r,      spawn,          SHCMD(TERMINAL " -e ranger --confdir $CONF/ranger") },  /* confdir param not really needed? */
+	{ CMDKEY,                       XK_t,      spawn,          SHCMD(TERMINAL) },
+	{ CMDKEY,                       XK_a,      spawn,          SHCMD("anki") },
+	{ CMDKEY,                       XK_s,      spawn,          SHCMD("spotify") },
+	/* { CMDKEY,                       XK_s,      spawn,          SHCMD("slack") }, */
+	{ CMDKEY,                       XK_f,      spawn,          SHCMD(TERMINAL " -e fzf") },
+	/* { MODKEY,                       XK_c,      spawn,          SHCMD(TERMINAL " -e cd $CONF && ls") }, */
+	{ CMDKEY,                       XK_c,      spawn,          SHCMD("sh $HOME/Documents/critical_driving_scenarios/project/carla/CarlaUE4.sh -opengl3 -quality-level=Low") },
+	{ CMDKEY,                       XK_v,      spawn,          SHCMD(TERMINAL " -e nvim -u ~/.config/nvim/init.vim") },
+	{ CMDKEY,                       XK_q,      killclient,     {0} },
+	{ CMDKEY,                       XK_b,      spawn,          SHCMD("qutebrowser") },
+	{ CMDKEY,                       XK_n,      spawn,          SHCMD(TERMINAL " -e task next") },
+	{ CMDKEY,                       XK_m,      spawn,          SHCMD(TERMINAL " -e gotop --color=solarized") },
+	/* { MODKEY,                       XK_m,      spawn,          SHCMD(TERMINAL " -e nvim -u $CONF/nvim/init.vim") }, */
+
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
@@ -76,6 +115,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
+ 	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[5]} },
+ 	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[6]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
