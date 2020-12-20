73648ec295712eb0a347d8480f7553bd2c35baaf
Vincent C. Mader <vincent.mader@icloud.com>
2020-12-20 22:51:21 +0100

    cleaned up qutebrowser config

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -2,112 +2,145 @@
 # =============================================================================
 #                           QuteBrowser config
 # =============================================================================
-#
+
 # config inspirations:
 #     * ii.com/qutebrowser-configpy
 
-# c.content.user_stylesheets = 'solarized.css'
-
 #                                                                       GENERAL
 # =============================================================================
-# always restore last session
-c.auto_save.session = True
-# do not require confirmation before quitting
+
+# start page
+c.url.start_pages = ['mader.xyz/qutebrowser']
+# default zoom level
+c.zoom.default = '200%'
+# downloads directory
+c.downloads.location.directory = '/home/vinc/downloads'
+
+# no confirmation before quit, because: always restore last session anyways
 c.confirm_quit = ['never']
-# automatically start playing videos
-c.content.autoplay = True
-# maximum number of items in cache
-# c.content.cache.maximum_pages = -1
+c.auto_save.session = True
+
+# maximum number of cache & undo items
+c.content.cache.maximum_pages = 1000
+c.tabs.undo_stack_size = -1  # undo items per tab: infinityyyyyyy
+
+# do not automatically start playing videos
+c.content.autoplay = False
 # allow websites to read canvas (needed for some sites)
 c.content.canvas_reading = True
+
 # cookies
 c.content.cookies.accept = 'all'
 c.content.cookies.store = True
-# maximum number of undo items to save (per tab)
-c.tabs.undo_stack_size = -1
-# disable video playback
-c.content.autoplay = False
 
 #                                                                SEARCH ENGINES
 # =============================================================================
 c.url.searchengines = {
-    # 'DEFAULT': 'https://duckduckgo.com/?q={}',
-    'DEFAULT': 'https://google.com/search?q={}',
-    'aw': 'https://wiki.archlinux.org/?search={}',
-    'gh': 'https://github.com/search?q={}',
-    'yt': 'https://youtube.com/results?search_query={}',
-    'tw': 'https://twitter.com/{}',
-    'r': 'https://reddit.com/r/{}',
-    '4': 'https://4chan.org/{}',
-    'w': 'https://en.wikipedia.org/?search={}',
-    'wt': 'https://en.wiktionary.org/?search={}',
-    'eb': 'https://ebay.com/sch/{}',
-    'ebk': 'https://ebay-kleinanzeigen.de/s-{}/k0',
-    'ud': 'https://urbandictionary.com/define.php?term={}',
-    'az': 'https://amazon.de/s?k={}',
-    # 'tpb': 'https://thepiratebay.org/search/{}',
+    # web search engines
+    'DEFAULT':  'https://google.com/search?q={}',
+    'ddg': 'https://duckduckgo.com/?q={}',
+    # encyclopedias
+    'aw':       'https://wiki.archlinux.org/?search={}',
+    'ud':       'https://urbandictionary.com/define.php?term={}',
+    'w':        'https://en.wikipedia.org/?search={}',
+    'wt':       'https://en.wiktionary.org/?search={}',
+    # social
+    'li':       'https://linkedin.com/search/results/all/?keywords={}',
+    'tw':       'https://twitter.com/{}',
+    # github
+    'gh':       'https://github.com/search?q={}',
+    'ghu':      'https://github.com/{}',
+    'ghv':      'https://github.com/vincentmader/{}',
+    # shopping
+    'az':       'https://amazon.de/s?k={}',
+    'eb':       'https://ebay.com/sch/{}',
+    'ebk':      'https://ebay-kleinanzeigen.de/s-{}/k0',
+    # misc
+    '4':        'https://4chan.org/{}',
+    'pb':       'https://piratebay.party/search/{}/1/99/0',
+    'r':        'https://reddit.com/r/{}',
+    'yt':       'https://youtube.com/results?search_query={}',
 }
 
 
-#                                                                    COMPLETION
+#                                                             COMPLETION WINDOW
 # =============================================================================
+
 # minimum number of chars needed to update completion list (for :open cmd)
 c.completion.min_chars = 1
-# height of window
-c.completion.height = '50%'
 # order of categories shown in the completion window
 c.completion.open_categories = [
     'searchengines', 'quickmarks', 'bookmarks', 'history'
 ]
 # quick-completion when there is only one entry
 c.completion.quick = True
-# padding (in px) of the scrollbar handle in the completion window
-c.completion.scrollbar.padding = 2
-# width of scrollbar handle in the completion window
-c.completion.scrollbar.width = 10
+
 # do not shrink the completion window to be smaller if there are no scrollbars
 c.completion.shrink = False
 # number of items to in history (-1 -> infinite)
 c.completion.web_history.max_items = -1
 # history items to hide in completion
 c.completion.web_history.exclude = []
-# default page
-# c.url.default_page = 'https://mynoise.net/Data/OCEAN/fb.jpg'
+
 #                                                                      MAPPINGS
 # =============================================================================
+
 # aliases
 c.aliases = {
-    'w': 'session-save', 'q': 'close', 'qa': 'quit',
-    'wq': 'quit --save', 'wqa': 'quit --save',
+    'w': 'session-save',
+    'wq': 'quit --save',
+    'wqa': 'quit --save',
+    'q': 'close',
+    'qa': 'quit',
 }
 # bindings
 c.bindings.commands['normal'] = {
-    # tab switching
     '<ctrl-tab>': 'tab-next',
     '<ctrl-shift-tab>': 'tab-prev',
     'zz': 'quit',
 }
-#                                                                        VISUAL
+
+#                                                                    APPEARANCE
 # =============================================================================
-# position of tab bar
+
+# set default fonts
+c.fonts.default_family = 'Hack Nerd Font'
+c.fonts.default_size = '20pt'
+c.fonts.completion.entry = '16pt Hack Nerd Font'
+c.fonts.statusbar = '16pt Hack Nerd Font'
+c.fonts.tabs.selected = '16pt Hack Nerd Font'
+c.fonts.tabs.unselected = '16pt Hack Nerd Font'
+c.fonts.hints = '20pt Hack Nerd Font'
+# set font paddings
+c.tabs.padding = {'bottom': 12, 'top': 12, 'right': 5, 'left': 5}
+c.statusbar.padding = {'bottom': 12, 'top': 12, 'right': 5, 'left': 5}
+# set font color for successfully loaded urls
+c.colors.statusbar.url.success.https.fg = 'white'
+
+# tab bar
 c.tabs.position = 'top'
+c.tabs.min_width = 300
+c.tabs.favicons.scale = 1.0
+c.tabs.title.format = '{audio} {current_title}'
+# completion window
+c.completion.height = '50%'
+c.completion.scrollbar.width = 10  # completion scrollbar handle width in px
+c.completion.scrollbar.padding = 2  # handle padding in px
+
 # render all pages with dark mode
+c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.prefers_color_scheme_dark = True
 # c.colors.webpage.darkmode.algorithm = 'lightness-cielab'
 # c.colors.webpage.darkmode.contrast = 0.0
 # c.colors.webpage.darkmode.grayscale.all = False
-# c.colors.webpage.darkmode.policy.images = 'never
+c.colors.webpage.darkmode.policy.images = 'never'
+c.colors.webpage.bg = '#222222'
+c.colors.tabs.odd.bg = '#222222'
+c.colors.tabs.even.bg = '#555555'
 
-
+# stylesheets
 # c.content.user_stylesheets = [
 #     './themes/solarized-dark.css',
 #     './themes/custom_solarized.css'
 # ]
-
-c.fonts.hints = '20pt Hack Nerd Font'  # TODO: increase font size
-c.fonts.default_size = '20pt'
-c.fonts.completion.entry = '18pt Hack Nerd Font'  # TODO: increase font size
-
-c.zoom.default = '200%'
-c.colors.statusbar.url.success.https.fg = 'white'
-
-c.fonts.default_family = 'Hack Nerd Font'
+# c.content.user_stylesheets = 'solarized.css'
