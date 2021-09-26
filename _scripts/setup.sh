30257a8ffabe6d89f2c51f8d655050dee2e4e210
Vincent Mader <vincent.mader@icloud.com>
2021-09-27 00:40:04 +0200

    minor

--- _scripts/setup.sh
+++ _scripts/setup.sh
@@ -1,3 +1,6 @@
 git clone git@github.com:emscripten-core/emsdk.git $CONF/emsdk
 git clone git@github.com:ohmyzsh/ohmyzsh.git $CONF/zsh/oh-my-zsh
 git clone git@github.com:alexanderjeurissen/ranger_devicons.git $CONF/ranger/plugins/ranger_devicons
+git clone git@github.com:zsh-users/zsh-syntax-highlighting.git $CONF/zsh/oh-my-zsh/plugins/zsh-syntax-highlighting
+
+nvim -c PlugInstall
