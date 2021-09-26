1c8f90e58b03ed0d8ea95f74e492c7b30dfaad94
Vincent C. Mader <vincent.mader@icloud.com>
2021-09-27 00:24:29 +0200

    removed external submodules, wrote script for auto-download

---
+++ _scripts/setup.sh
@@ -0,0 +1,3 @@
+git clone git@github.com:emscripten-core/emsdk.git $CONF/emsdk
+git clone git@github.com:ohmyzsh/ohmyzsh.git $CONF/zsh/oh-my-zsh
+git clone git@github.com:alexanderjeurissen/ranger_devicons.git $CONF/ranger/plugins/ranger_devicons
