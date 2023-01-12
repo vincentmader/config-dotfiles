752c80726d8a5308d24c37d34dd5d94d7c3843ad
Vincent C. Mader <vincent.mader@icloud.com>
2023-01-12 13:24:59 +0100

    moved over plugins from old nvim config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,3 +1,26 @@
 return require("packer").startup(function() 
     use "wbthomason/packer.nvim"
+
+    use "tpope/vim-surround"
+    use "tpope/vim-commentary"
+    use "tpope/vim-fugitive"
+
+    use { "neoclide/coc.nvim", branch="release" }
+
+    use { "nvim-telescope/telescope.nvim", 
+        requires = {
+            "nvim-lua/plenary.nvim"
+        }
+    }
+
+    use "machakann/vim-highlightedyank"
+
+    use "wellle/tmux-complete.vim"
+    use "christoomey/vim-tmux-navigator"
+
+    use "altercation/vim-colors-solarized"
+    use "ryanoasis/vim-devicons"
+
+    use "junegunn/goyo.vim"
+    use "junegunn/limelight.vim"
 end)
